8dc9ef04aec266096435d473e3af810b9b762462
unknown <hugo.levy@gmail.com>
2021-02-10 10:54:21 +0100

    ajout colorbar aux animations

--- arch/filament/display/animate.py
+++ arch/filament/display/animate.py
@@ -1,22 +1,35 @@
 import matplotlib.pyplot as plt
 from matplotlib.animation import ArtistAnimation
+from matplotlib.cm import ScalarMappable
+from matplotlib.colors import Normalize
+import numpy as np
 from netCDF4 import Dataset
 from IPython.display import HTML
 from base64 import b64encode
 import os
 
-def make_video(pathCDF, save_path, variable):
+def make_video(pathCDF, save_path, variable, cmap='magma'):
     try:
         os.mkdir(save_path+'/videos')
     except FileExistsError:
         pass
     
     resultsCDF = Dataset(pathCDF, 'r', format='NETCDF4', parallel=False)
+    
+    min_value = np.min(resultsCDF[variable][:].data)
+    max_value = np.max(resultsCDF[variable][:].data)
 
     fig = plt.figure(figsize=(12,8))
+    ax = plt.subplot(111)
+    ax.set_title(variable, fontsize=25, pad=15)
+    cbar = fig.colorbar(ScalarMappable(cmap=cmap, norm=Normalize(vmin=min_value, vmax=max_value)), ax=ax)
+    cbar_ticks = cbar.get_ticks()
+    cbar.set_ticks(cbar_ticks)
     frames = [[plt.imshow(resultsCDF[variable][:,:,iteration_nb].T, 
                           origin='lower', 
-                          cmap='magma')]
+                          cmap=cmap,
+                          vmin=min_value,
+                          vmax=max_value)]
               for iteration_nb in range(resultsCDF.dimensions['Nt'].size)]
     resultsCDF.close()
     anim = ArtistAnimation(fig, frames, interval=200, blit=False, repeat=False)
