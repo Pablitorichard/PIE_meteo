68e8d287840037f304821962967815fd758a1328
Olivier Goux <olivier.goux@student.isae-supaero.fr>
2021-02-16 18:53:22 +0100

    Succesful test for optimized versions of the interpolation code

--- filament_modeling/test_interpolation.py
+++ filament_modeling/test_interpolation.py
@@ -12,6 +12,7 @@ import time
 
 Nx = 256
 Ny = 128
+
 ratio_x = Nx//10 # 1 arrow every <ratio> point
 ratio_y = Ny//10
 [x_grid, y_grid] = np.mgrid[0:Nx, 0:Ny]
@@ -20,7 +21,7 @@ F = np.zeros((Nx, Ny))
 bubble_indices = np.where ( (x_grid - Nx//2)**2 +
                                 (y_grid - Ny//2)**2 < 20**2 )
 [X,Y] = np.mgrid[0:Nx,0:Ny]
-F = 300*np.ones((Nx, Ny))
+F = np.zeros((Nx, Ny))
 depth = -15
 # Half width of the stripe
 dY = Ny//10
@@ -36,23 +37,22 @@ left_ind= np.where(np.logical_and(\
                 (X - dX)**2 + (Y - Ny//2)**2 < (dY)**2,
                 X < dX ))
 F[left_ind] = F[left_ind] +\
-         (np.sqrt((Y[left_ind]-Ny//2)**2 + (X[left_ind]-dX)**2 ) \
-         /dY -1)*depth
+          (np.sqrt((Y[left_ind]-Ny//2)**2 + (X[left_ind]-dX)**2 ) \
+          /dY -1)*depth
              
 right_ind= np.where(np.logical_and(\
                 (X - (Nx - dX - 1))**2 + (Y - Ny//2)**2 < dY**2,
                 X > (Nx - dX - 1) ))
 F[right_ind] = F[right_ind] +\
-         (np.sqrt((Y[right_ind]-Ny//2)**2 + \
+          (np.sqrt((Y[right_ind]-Ny//2)**2 + \
                   (X[right_ind]-(Nx-dX-1))**2 )/dY -1)*depth
-#F[60:65,60:65]=1
 
-# alpha_x = 0.5*np.ones((Nx,Ny)) + 10*np.random.rand(Nx,Ny)
-# alpha_y = 0.5*np.ones((Nx,Ny)) + 10*np.random.rand(Nx,Ny)
 
-alpha_x = 30.5*np.ones((Nx,Ny)) 
-alpha_y = 30.5*np.ones((Nx,Ny))
+#F[-1,-1] = 1
 
+
+alpha_x = 30.75*np.ones((Nx,Ny)) 
+alpha_y = 20.25*np.ones((Nx,Ny))
 """
 Problem: Finding the field F_upstream such that its value in (x,y) 
          represents the value of the field F in (x-alpha_x, y-alpha_y) 
@@ -64,32 +64,34 @@ t1=time.time() - t0
 
 t0=time.time()
 F_linear = upstream_interp(alpha_x, alpha_y, F, method='linear')
-# F_failure = interpolate.griddata(
-#             (x_grid.flatten(), y_grid.flatten()), F.flatten(),
-#             (x_grid - alpha_x, y_grid - alpha_y),
-#             method='cubic',fill_value=0) 
 t2=time.time() - t0
 
+t0=time.time()
+F_cubic_V1 = upstream_interp(alpha_x, alpha_y, F, method='bicubic_V1')
+t3=time.time() - t0
+
+t0=time.time()
+F_linear_V2 = upstream_interp(alpha_x, alpha_y, F, method='linear_V2')
+t4=time.time() - t0
 
 plt.subplot(2,2,1)
-plt.pcolormesh(F.T)
+plt.pcolormesh(F_cubic.T)
 plt.colorbar()
-plt.xlabel('Before interpolation')
+plt.xlabel('Cubic naive, dt =' + str(round(t1,6)))
+
 plt.subplot(2,2,2)
+plt.pcolormesh(F_cubic_V1.T)
+plt.colorbar()
+plt.xlabel('Cubic optimized , dt =' + str(round(t3,6)))
+
+plt.subplot(2,2,3)
 plt.pcolormesh(F_linear.T)
 plt.colorbar()
-plt.quiver(x_grid[0:Nx:ratio_x,0],y_grid[0,0:Ny:ratio_y],
-                  alpha_x[0:Nx:ratio_x, 0:Ny:ratio_y],
-                  alpha_y[0:Nx:ratio_x, 0:Ny:ratio_y],
-                  color='b', width=0.003)
-plt.xlabel('Linear interpolation, dt = ' + str(round(t2,4)))
-plt.subplot(2,2,3)
-plt.pcolormesh(F_cubic.T)
-plt.xlabel('Cubic interpolation, dt = ' + str(round(t1,4)))
-plt.colorbar()
-plt.subplot(2,2,4)
-plt.pcolormesh((F_cubic-F_linear).T)
-plt.colorbar()
-plt.xlabel('Cubic - Linear')
+plt.xlabel('Linear naive, dt = ' + str(round(t2,6)))
 
-print("Computation time for upstream_interp: ", t1, " ; for griddata: ",t2)
+
+plt.subplot(2,2,4)
+plt.pcolormesh(F_linear_V2.T)
+plt.colorbar()
+plt.xlabel('Linear optimized , dt = ' + str(round(t4,6)))
+
