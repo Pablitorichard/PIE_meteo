cd7a387430987a7c75d75eba932da6bba5c0c198
Olivier Goux <olivier.goux@student.isae-supaero.fr>
2021-02-16 21:25:44 +0100

    Fixed some small (but critical) errors in optimized interpolation methods

--- filament_modeling/test_interpolation.py
+++ filament_modeling/test_interpolation.py
@@ -10,8 +10,8 @@ from upstream_interp import upstream_interp
 import matplotlib.pyplot as plt
 import time
 
-Nx = 256
-Ny = 128
+Nx = 20
+Ny = 10
 
 ratio_x = Nx//10 # 1 arrow every <ratio> point
 ratio_y = Ny//10
@@ -21,12 +21,12 @@ F = np.zeros((Nx, Ny))
 bubble_indices = np.where ( (x_grid - Nx//2)**2 +
                                 (y_grid - Ny//2)**2 < 20**2 )
 [X,Y] = np.mgrid[0:Nx,0:Ny]
-F = np.zeros((Nx, Ny))
+
 depth = -15
 # Half width of the stripe
 dY = Ny//10
 # Distance between the side of the X axis and the first V-profile
-dX = Nx//8
+dX = Nx//6
 
 
 F[dX : Nx-dX, Ny//2 - dY : Ny//2 + dY] = \
@@ -48,11 +48,18 @@ F[right_ind] = F[right_ind] +\
                   (X[right_ind]-(Nx-dX-1))**2 )/dY -1)*depth
 
 
-#F[-1,-1] = 1
+#F[Nx//2: Nx//2+1, Ny//2: Ny//2 +1] = 1
 
+# F = np.tile(F, (2,1,1))
+# F[0,...] *= 2
 
-alpha_x = 30.75*np.ones((Nx,Ny)) 
-alpha_y = 20.25*np.ones((Nx,Ny))
+[X, Y] = np.mgrid[0:Nx, 0:Ny]
+
+alpha_x = 0.2*np.random.randint(5, size=(Nx,Ny))
+alpha_y = 0.65*np.random.randint(5, size=(Nx,Ny))
+# alpha_x = 0.9 * np.ones((Nx,Ny))
+# alpha_x[X>Nx//2] *= 0
+# alpha_y = 0*np.ones((Nx,Ny))
 """
 Problem: Finding the field F_upstream such that its value in (x,y) 
          represents the value of the field F in (x-alpha_x, y-alpha_y) 
@@ -63,35 +70,36 @@ F_cubic = upstream_interp(alpha_x, alpha_y, F, method='bicubic')
 t1=time.time() - t0
 
 t0=time.time()
-F_linear = upstream_interp(alpha_x, alpha_y, F, method='linear')
+F_cubic_V1 = upstream_interp(alpha_x, alpha_y, F, method='bicubic_V1')
 t2=time.time() - t0
 
 t0=time.time()
-F_cubic_V1 = upstream_interp(alpha_x, alpha_y, F, method='bicubic_V1')
+F_cubic_V2 = upstream_interp(alpha_x, alpha_y, F, method='bicubic_V2')
 t3=time.time() - t0
 
-t0=time.time()
-F_linear_V2 = upstream_interp(alpha_x, alpha_y, F, method='linear_V2')
-t4=time.time() - t0
+
 
 plt.subplot(2,2,1)
-plt.pcolormesh(F_cubic.T)
-plt.colorbar()
-plt.xlabel('Cubic naive, dt =' + str(round(t1,6)))
+plt.pcolormesh(F.T)
+
 
 plt.subplot(2,2,2)
-plt.pcolormesh(F_cubic_V1.T)
+plt.pcolormesh(F_cubic.T)
 plt.colorbar()
-plt.xlabel('Cubic optimized , dt =' + str(round(t3,6)))
+plt.xlabel('cubic naive, dt = ' + str(round(t1,6)))
+
 
 plt.subplot(2,2,3)
-plt.pcolormesh(F_linear.T)
+plt.pcolormesh(F_cubic_V1.T)
 plt.colorbar()
-plt.xlabel('Linear naive, dt = ' + str(round(t2,6)))
+plt.xlabel('cubic optimized 1, dt = ' + str(round(t2,6)))
 
 
 plt.subplot(2,2,4)
-plt.pcolormesh(F_linear_V2.T)
+plt.pcolormesh(F_cubic_V2.T)
 plt.colorbar()
-plt.xlabel('Linear optimized , dt = ' + str(round(t4,6)))
+plt.xlabel('cubic optimized 2 = ' + str(round(t3,6)))
 
+
+
+print (np.max(np.abs(F_cubic_V2 - F_cubic)))
