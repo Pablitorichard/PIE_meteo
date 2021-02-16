91648f6b8044a1e942de554c8b56ff0195138d21
unknown <hugo.levy@gmail.com>
2021-02-16 19:10:31 +0100

    minor modif to animations

--- arch/filament/display/animate.py
+++ arch/filament/display/animate.py
@@ -15,24 +15,51 @@ def make_video(pathCDF, save_path, variable, cmap='magma'):
         pass
     
     resultsCDF = Dataset(pathCDF, 'r', format='NETCDF4', parallel=False)
-    
+
+    # Get min and max value for the colorbar
     min_value = np.min(resultsCDF[variable][:].data)
     max_value = np.max(resultsCDF[variable][:].data)
 
+    ## Figure Options ##
+    
     fig = plt.figure(figsize=(12,8))
     ax = plt.subplot(111)
-    ax.set_title(variable, fontsize=25, pad=15)
+    
+    ax.set_title(var2str(variable), fontsize=25, pad=15)
+    
+    # Color bar creation (static one)
     cbar = fig.colorbar(ScalarMappable(cmap=cmap, norm=Normalize(vmin=min_value, vmax=max_value)), ax=ax)
     cbar_ticks = cbar.get_ticks()
     cbar.set_ticks(cbar_ticks)
-    frames = [[plt.imshow(resultsCDF[variable][:,:,iteration_nb].T, 
+    
+    # Axis tweaks
+    ax.tick_params(labelsize=15, direction='in', length=10, width=1, pad=5, color='white')
+    ax.set_ylabel(r'y Axis $(km)$', fontsize=15)
+    ax.set_xlabel(r'x Axis $(km)$', fontsize=15)
+    
+    frames = []
+    
+    for iteration_nb in range(resultsCDF.dimensions['Nt'].size):
+    
+        # Creating the figure
+        myFig = plt.imshow(resultsCDF[variable][:,:,iteration_nb].T, 
                           origin='lower', 
                           cmap=cmap,
                           vmin=min_value,
-                          vmax=max_value)]
-              for iteration_nb in range(resultsCDF.dimensions['Nt'].size)]
+                          vmax=max_value)
+                          
+        # Info on elapsed time
+        myTitle = 'Elapsed Time = {hour:2d}'.format(hour=int(resultsCDF['t'][iteration_nb]/3600)) + ' hours'
+        subtitle = ax.text(0.1,-0.15,myTitle,
+                        size=plt.rcParams["axes.titlesize"],
+                        ha="center", transform=ax.transAxes)
+                        
+        # Add the pair to the list of frames
+        frames.append([myFig, subtitle])
+
     resultsCDF.close()
     anim = ArtistAnimation(fig, frames, interval=200, blit=False, repeat=False)
+    
     anim.save(save_path+'/videos/'+variable+'.mp4')
     plt.close()
     
@@ -41,3 +68,20 @@ def make_video(pathCDF, save_path, variable, cmap='magma'):
     return HTML("""<video width=800 controls>
                          <source src="%s" type="video/mp4">
                    </video>""" % data_url)
+                   
+def var2str(var_name):
+
+    switcher_name={
+        'ut':r'$u_t \ (m.s^{-1})$',
+        'vt':r'$v_t \ (m.s^{-1})$',
+        'theta_t':r"$\theta^{'}_{tp} \ (K)$",
+        'us':r'$u_s \ (m.s^{-1})$',
+        'v_s':r'$v_s \ (m.s^{-1})$',
+        'w':r'$w \ (m.s^{-1})$',
+        'Delta_z':r'$\Delta z \ (m)$',
+        'Delta_T_bb':r'$\Delta T_{bb} \ (K)$',
+        'Delta_T_hist':r'$\Delta T_{bb}^{hist} \ (K)$'
+    }
+    
+    return switcher_name.get(var_name, "Invalid Variable")
+
