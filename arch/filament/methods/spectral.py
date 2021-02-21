f280d1b507af43f2405b7c9a8e9150380ac433de
Mathieur14 <mathieu.roule@polytechnique.edu>
2021-02-21 12:27:50 +0100

    Optimisation des methodes spectrales

--- arch/filament/methods/spectral.py
+++ arch/filament/methods/spectral.py
@@ -1,32 +1,33 @@
 import numpy as np
 
-def geostwind(a, b, thetatp, z=0, fourier=False, verbose=0):
+def geostwind(a, b, thetatp, params, z=0, fourier=False, verbose=0):
     
     f = 1e-4
-    theta00 = 300
-    g = 10
-    Ns = 2e-2
-    Nt = 1e-2
+    theta00 = params['theta_00']
+    g = params['g']
+    Ns = params['N_s']
+    Nt = params['N_t']
+    N = Ns if z>0 else Nt
     pate = g*(Ns-Nt)/(theta00*Ns*Nt)
     
     Pa, Pb = thetatp.shape
     
     thetatphat = np.fft.fft2(thetatp)
-        
     freqx = np.fft.fftfreq(Pa, a/Pa)
     freqy = np.fft.fftfreq(Pb, b/Pb)
-    wavevect = np.array([[(2*np.pi*m,2*np.pi*n) for n in freqy] for m in freqx])
     
-    N = Ns if z>0 else Nt
+    vecFreqX = 2*np.pi*freqx # 1 seule dim 
+    vecFreqY = 2*np.pi*freqy # idem
+    
+    KmatX, KmatY = np.meshgrid(vecFreqX, vecFreqY)
+    Kmat = np.sqrt(KmatX**2 + KmatY**2).T
+    
+    Kmat[np.where(Kmat==0)]=float('Inf')
+    
+    Mat = pate/Kmat if (z==0) else pate/Kmat * np.exp(-N*Kmat/f*np.abs(z))
     psihat = thetatphat
-    for m in range(Pa):
-        for n in range(Pb):
-            K = np.linalg.norm(wavevect[m,n])
-            if K==0:
-                psihat[m,n] = 0
-            else:
-                psihat[m,n] *= pate/K*np.exp(-N*K/f*np.abs(z))
 
+    psihat *= Mat
     if fourier:
         ug = -np.fft.ifft2(psihat*1j*wavevect[:,:,1]).real
         vg = np.fft.ifft2(psihat*1j*wavevect[:,:,0]).real
@@ -35,16 +36,17 @@ def geostwind(a, b, thetatp, z=0, fourier=False, verbose=0):
         psi = np.fft.ifft2(psihat).real
         ug = -(np.roll(psi,-1,1)-np.roll(psi,1,1))/(2*a/Pa)
         vg = (np.roll(psi,-1,0)-np.roll(psi,1,0))/(2*b/Pb)
-    
+
     return ug, vg
 
-def vertwind(a, b, thetatp, thetatpprev, dt, z=0, verbose=0):
+def vertwind(a, b, thetatp, thetatpprev, dt, params, z=0, verbose=0):
     
     f = 1e-4
-    theta00 = 300
-    g = 10
-    Ns = 2e-2
-    Nt = 1e-2
+    theta00 = params['theta_00']
+    g = params['g']
+    Ns = params['N_s']
+    Nt = params['N_t']
+    N = Ns if z>0 else Nt
     pate = g*(Ns-Nt)/(theta00*Ns*Nt)
     
     Pa, Pb = thetatp.shape
@@ -56,24 +58,21 @@ def vertwind(a, b, thetatp, thetatpprev, dt, z=0, verbose=0):
     freqy = np.fft.fftfreq(Pb, b/Pb)
     wavevect = np.array([[(2*np.pi*m,2*np.pi*n) for n in freqy] for m in freqx])
     
-    N = Ns if z>0 else Nt
+    vecFreqX = 2*np.pi*freqx # 1 seule dim 
+    vecFreqY = 2*np.pi*freqy # idem
+    
+    KmatX, KmatY = np.meshgrid(vecFreqX, vecFreqY)
+    Kmat = np.sqrt(KmatX**2 + KmatY**2).T
+    Kmat[np.where(Kmat==0)]=float('Inf') # set to inf. null wavenumbers
+    
     psihat = thetatphat
     psiprevhat = thetatpprevhat
-    thetazhat = np.zeros(thetatphat.shape,dtype=complex)
-    thetazprevhat = np.zeros(thetatpprevhat.shape,dtype=complex)
-    for m in range(Pa):
-        for n in range(Pb):
-            K = np.linalg.norm(wavevect[m,n])
-            if K==0:
-                psihat[m,n] = 0
-                psiprevhat[m,n] = 0
-                thetazhat[m,n] = 0
-                thetazprevhat[m,n] = 0
-            else:
-                psihat[m,n] *= pate/K*np.exp(-N*K/f*np.abs(z))
-                psiprevhat[m,n] *= pate/K*np.exp(-N*K/f*np.abs(z))
-                thetazhat[m,n] = theta00/g*(-np.sign(z))*N*K*psihat[m,n]
-                thetazprevhat[m,n] = theta00/g*(-np.sign(z))*N*K*psiprevhat[m,n]
+    Mat = pate/Kmat if (z==0) else pate/Kmat * np.exp(-N*Kmat/f*np.abs(z))
+    psihat       *= Mat
+    psiprevhat   *= Mat
+    Kmat[np.where(np.isinf(Kmat))]=0 # set inf. elements back to 0
+    thetazhat     = theta00/g*(-np.sign(z))*N*Kmat*psihat
+    thetazprevhat = theta00/g*(-np.sign(z))*N*Kmat*psiprevhat
 
     ug = -np.fft.ifft2(psihat*1j*wavevect[:,:,1]).real
     vg = np.fft.ifft2(psihat*1j*wavevect[:,:,0]).real
